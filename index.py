52a45607356ff7e2148a1879ea263e17a225d050
Felipe Guimarães <146668592+Fezaoo@users.noreply.github.com>
2024-04-01 11:55:27 -0300

    Bug fix
    All emails were being flagged as 'Seen' because the script had to open them.

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import imaplib
 import email
+from email.utils import parseaddr
 
 def marcar_emails_como_lidos(usuario, senha, filtro_remetente):
     # Conectar ao servidor IMAP do Gmail
@@ -11,18 +12,30 @@ def marcar_emails_como_lidos(usuario, senha, filtro_remetente):
     resultado, data = conexao.search(None, 'UNSEEN')  # Apenas e-mails não lidos
     print(resultado)
     ids = data[0].split()
+    ids.reverse()
+
+    contador = 0
 
     for id in ids:
+        if contador >= 50:
+          break
+
         print(id)
         resultado, data = conexao.fetch(id, '(RFC822)')
         mensagem = email.message_from_bytes(data[0][1])
 
         # Verificar filtros por remetente
-        remetente = mensagem['From']
+        remetente = parseaddr(mensagem['From'])[1]
         print(remetente)
-        if filtro_remetente in remetente:
+        print(filtro_remetente)
+        if filtro_remetente == remetente:
             # Marcar e-mail como lido
             conexao.store(id, '+FLAGS', '\\Seen')
+            print('\033[32m Marcado como lido com sucesso \033[m')
+        else:
+            conexao.store(id, '-FLAGS', '\\Seen')
+        contador += 1
+
 
     # Fechar conexão
     conexao.close()
