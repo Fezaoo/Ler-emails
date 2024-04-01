93c7d5c69c6601948f28166573a8304d9dd184d8
Felipe Guimarães <146668592+Fezaoo@users.noreply.github.com>
2024-04-01 15:06:57 -0300

    documentação

--- index.py
+++ index.py
@@ -3,49 +3,49 @@ import email
 from email.utils import parseaddr
 
 def marcar_emails_como_lidos(usuario, senha, filtro_remetente):
-    # Conectar ao servidor IMAP do Gmail
+    # Conecta ao servidor IMAP do Gmail
     conexao = imaplib.IMAP4_SSL('imap.gmail.com')
     conexao.login(usuario, senha)
     conexao.select('inbox')
 
-    # Buscar e-mails na caixa de entrada
+    # Busca e-mails na caixa de entrada
     resultado, data = conexao.search(None, 'UNSEEN')  # Apenas e-mails não lidos
-    print(resultado)
     ids = data[0].split()
     ids.reverse()
-
+    print(f'Resultado da conexão: {resultado}')
+    print(f'')
+    limite = 0
     contador = 0
-
     for id in ids:
-        if contador >= 50:
+        if limite >= 20: # Limite para ler 50 emails
           break
 
-        print(id)
         resultado, data = conexao.fetch(id, '(RFC822)')
         mensagem = email.message_from_bytes(data[0][1])
 
         # Verificar filtros por remetente
         remetente = parseaddr(mensagem['From'])[1]
-        print(remetente)
-        print(filtro_remetente)
         if filtro_remetente == remetente:
             # Marcar e-mail como lido
             conexao.store(id, '+FLAGS', '\\Seen')
-            print('\033[32m Marcado como lido com sucesso \033[m')
+            contador +=1
         else:
+            # Marca e-mail como não lido caso não seja o remetente alvo
             conexao.store(id, '-FLAGS', '\\Seen')
-        contador += 1
+        limite += 1
 
 
-    # Fechar conexão
+    # Fecha conexão
     conexao.close()
     conexao.logout()
+    print(f'\033[32m {contador} Emails marcados como lido \033[m')
 
 # Configurações
+## É importante lembrar que, caso sua conta tenha verificação duas etapas, será necessário configurar uma senha para aplicativo na sessão autenticação por duas etapas. 
 usuario = 'fe.guimaraes972@gmail.com'
-senha = 'wgozvjjlcqfkxncf'
-filtro_remetente = 'jobalerts-noreply@linkedin.com'
+senha = 'wgozvjjlcqfkxncf' # Senha do email ou senha para aplicativo
+filtro_remetente = 'jobalerts-noreply@linkedin.com' # Remetente alvo
 
-# Executar a função para marcar e-mails como lidos
+# Executa a função para marcar e-mails como lidos
 marcar_emails_como_lidos(usuario, senha, filtro_remetente)
 print('Terminado')
